76f499430d7bbee56468bf5690a574c1c75985d5
mizuirostudio <magmidori@gmail.com>
2024-04-15 14:45:49 +0900

    url修正

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
-    <ul class="footer-nav-links">
-      <li class="footer-nav-item"><a href="index.php">&copy; みずいろスタジオ</a></li>
-      <li class="footer-nav-item"><a href="privacy-policy.php">プライバシーポリシー</a></li>
-    </ul>
-  </footer>
+  <ul class="footer-nav-links">
+    <li class="footer-nav-item"><a href="https://mizuirostudio.com/">&copy; みずいろスタジオ</a></li>
+    <li class="footer-nav-item"><a href="privacy-policy.php">プライバシーポリシー</a></li>
+  </ul>
+</footer>
 </body>
 
 </html>
